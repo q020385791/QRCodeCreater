8466b83e7d1649437fd23e0286144a3b9c315527
sai wanza <q020385791@gmail.com>
2021-06-15 15:24:16 +0800

    加入專案檔案。

---
+++ QRCodeCreater/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace QRCodeCreater
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateQRCode = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreateQRCode
+            // 
+            this.btnCreateQRCode.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateQRCode.Name = "btnCreateQRCode";
+            this.btnCreateQRCode.Size = new System.Drawing.Size(99, 34);
+            this.btnCreateQRCode.TabIndex = 0;
+            this.btnCreateQRCode.Text = "Create";
+            this.btnCreateQRCode.UseVisualStyleBackColor = true;
+            this.btnCreateQRCode.Click += new System.EventHandler(this.btnCreateQRCode_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(117, 16);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(181, 30);
+            this.txtInput.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCreateQRCode);
+            this.Name = "Form1";
+            this.Text = "QRCodeCreater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateQRCode;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ QRCodeCreater/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;  //使用套件ZXing
+using ZXing.Common;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+
+namespace QRCodeCreater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreateQRCode_Click(object sender, EventArgs e)
+        {
+
+            //娶得主目錄下的logo.png檔案
+            Bitmap logo = new Bitmap(Directory.GetCurrentDirectory()+@"\logo.png");
+            var writer = new BarcodeWriter  //dll裡面可以看到屬性
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions //設定QRCode大小
+                {
+                    Height = 300,
+                    Width = 300,
+                }
+            };
+
+            string QRCodeSource = txtInput.Text;
+            if (string.IsNullOrEmpty(QRCodeSource))
+            {
+                MessageBox.Show("尚未填入轉換資料來源");
+                return;
+            }
+            //產生QRcode
+
+            #region 帶logo
+            MultiFormatWriter Muwriter = new MultiFormatWriter();
+            Dictionary<EncodeHintType, object> hint = new Dictionary<EncodeHintType, object>();
+            hint.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+            hint.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            BitMatrix bm = Muwriter.encode(QRCodeSource, BarcodeFormat.QR_CODE, 300, 300, hint);
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            Bitmap map = barcodeWriter.Write(bm);
+            //得到二維碼實際尺寸
+            int[] rectangle = bm.getEnclosingRectangle();
+            //計算插入圖片之位置與大小
+            int middleW = Math.Min((int)(rectangle[2] / 3.5), logo.Width);
+            int middleH = Math.Min((int)(rectangle[3] / 3.5), logo.Height);
+            int middleL = (map.Width - middleW) / 2;
+            int middleT = (map.Height - middleH) / 2;
+
+            Bitmap bmpimg = new Bitmap(map.Width, map.Height, PixelFormat.Format32bppArgb);
+            using (Graphics g = Graphics.FromImage(bmpimg))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                g.DrawImage(map, 0, 0);
+            }
+            //将二维码插入图片
+            Graphics myGraphic = Graphics.FromImage(bmpimg);
+            //白底
+            myGraphic.FillRectangle(Brushes.White, middleL, middleT, middleW, middleH);
+            myGraphic.DrawImage(logo, middleL, middleT, middleW, middleH);
+
+
+
+
+            #endregion
+
+
+            #region 不帶logo
+
+            //var img = writer.Write(QRCodeSource);
+            //string FileName = "ithome";
+            //Bitmap bmpimg = new Bitmap(img);
+
+            #endregion
+            pictureBox1.Image = bmpimg;
+        }
+    }
+}
